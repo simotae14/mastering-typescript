f08c78a105b129c4b2ee96184665d0bcd2c4883e
Simone Taeggi <startae14@gmail.com>
2022-08-09 09:48:03 +0200

    Declare variables

---
+++ Types/variables.js
@@ -0,0 +1,14 @@
+var movieTitle = 'Amadeus';
+movieTitle = 'Arrival';
+// movieTitle = 9;
+movieTitle.toUpperCase();
+var numCatLives = 9;
+numCatLives += 1;
+// numCatLives = 'zero'
+var gameOver = false;
+gameOver = true;
+// gameOver = 'true';
+null;
+undefined;
+var nothing = null;
+var foo = undefined;
---
+++ Types/variables.ts
@@ -0,0 +1,17 @@
+let movieTitle: string = 'Amadeus';
+movieTitle = 'Arrival';
+// movieTitle = 9;
+movieTitle.toUpperCase();
+
+let numCatLives: number = 9;
+numCatLives += 1;
+// numCatLives = 'zero'
+
+let gameOver: boolean = false;
+gameOver = true;
+// gameOver = 'true';
+
+null
+undefined
+let nothing: null = null;
+let foo: undefined = undefined;
