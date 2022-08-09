fc3c7c63bdb4c70ddcc4f55127032d11c81937c2
Simone Taeggi <startae14@gmail.com>
2022-08-09 09:47:38 +0200

    Definition variables

---
+++ basics.js
@@ -0,0 +1,4 @@
+var pi = 3.14159;
+console.log("HELLO!");
+console.log("GOODBYE!!");
+console.log(Math.round(7.382398575983));
---
+++ basics.ts
@@ -0,0 +1,5 @@
+let pi: number = 3.14159;
+
+console.log("HELLO!");
+console.log("GOODBYE!!");
+console.log(Math.round(7.382398575983));
