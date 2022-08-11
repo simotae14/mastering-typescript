618f0a70ec545c31b94ed8bb691d99ad963d5efd
Simone Taeggi <startae14@gmail.com>
2022-08-11 11:30:05 +0200

    add a Default value for arguments in a function

--- functions.js
+++ functions.js
@@ -1,10 +1,13 @@
 function square(num) {
     return num * num;
 }
-function greet(person) {
-    return "Hi there, ".concat(person, "!");
-}
 var doSomething = function (person, age, isFunny) { };
 square(3);
 greet('simo');
 doSomething("ChickenFace", 45, false);
+// default value
+function greet(person) {
+    if (person === void 0) { person = 'stranger'; }
+    return "Hi there, ".concat(person, "!");
+}
+greet();
--- functions.ts
+++ functions.ts
@@ -2,12 +2,15 @@ function square(num: number) {
     return num * num;
 }
 
-function greet(person: string) {
-    return `Hi there, ${person}!`;
-}
-
 const doSomething = (person: string, age: number, isFunny: boolean) => {};
 
 square(3);
 greet('simo');
 doSomething("ChickenFace", 45, false);
+
+// default value
+function greet(person: string = 'stranger') {
+    return `Hi there, ${person}!`;
+}
+
+greet();
