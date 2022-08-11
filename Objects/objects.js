e542831a9aa4424cf0cdc402a52ff24c43b04fdb
Simone Taeggi <startae14@gmail.com>
2022-08-11 17:20:12 +0200

    Define an object variable and an object returned type definition

--- Objects/objects.js
+++ Objects/objects.js
@@ -2,3 +2,9 @@ function printName(person) {
     console.log("".concat(person.first, " ").concat(person.last));
 }
 printName({ first: "Thomas", last: "Jenkins" });
+// define an object variable
+var coordinate = { x: 34, y: 2 };
+// define an object return
+function randomCoordinate() {
+    return { x: Math.random(), y: Math.random() };
+}
--- Objects/objects.ts
+++ Objects/objects.ts
@@ -3,3 +3,11 @@ function printName(person: { first: string; last: string }): void {
 }
 
 printName({ first: "Thomas", last: "Jenkins" });
+
+// define an object variable
+let coordinate: { x: number; y: number } = { x: 34, y: 2 };
+
+// define an object return
+function randomCoordinate(): { x: number; y: number } {
+    return { x: Math.random(), y: Math.random() };
+}
