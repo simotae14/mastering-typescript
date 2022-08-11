c99c6f8f43be97f89ab9a172bf717ef80ef3d617
Simone Taeggi <startae14@gmail.com>
2022-08-11 11:22:32 +0200

    denine Types of function arguments

---
+++ functions.js
@@ -0,0 +1,8 @@
+function square(num) {
+    return num * num;
+}
+function greet(person) {
+    return "Hi there, ".concat(person, "!");
+}
+square(3);
+greet('simo');
---
+++ functions.ts
@@ -0,0 +1,10 @@
+function square(num: number) {
+    return num * num;
+}
+
+function greet(person: string) {
+    return `Hi there, ${person}!`;
+}
+
+square(3);
+greet('simo');
