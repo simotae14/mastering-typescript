7244321fa4b718ecd06aecb9241765cef06a4090
Simone Taeggi <startae14@gmail.com>
2022-08-11 11:25:38 +0200

    Defining more arguments types in a function

--- functions.js
+++ functions.js
@@ -4,5 +4,7 @@ function square(num) {
 function greet(person) {
     return "Hi there, ".concat(person, "!");
 }
+var doSomething = function (person, age, isFunny) { };
 square(3);
 greet('simo');
+doSomething("ChickenFace", 45, false);
--- functions.ts
+++ functions.ts
@@ -6,5 +6,8 @@ function greet(person: string) {
     return `Hi there, ${person}!`;
 }
 
+const doSomething = (person: string, age: number, isFunny: boolean) => {};
+
 square(3);
 greet('simo');
+doSomething("ChickenFace", 45, false);
