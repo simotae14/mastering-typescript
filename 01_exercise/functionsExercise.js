29026b9204a292f902e1c560640e758e75eba2f2
Simone Taeggi <startae14@gmail.com>
2022-08-11 16:30:25 +0200

    Function Types EXERCISE

---
+++ 01_exercise/functionsExercise.js
@@ -0,0 +1,31 @@
+// **********************************************
+// ******************* PART 1 *******************
+// **********************************************
+// Write a function called "twoFer" that accepts a person's name
+// It should return a string in the format "one for <name>, one for me"
+// If no name is provided, it should default to "you"
+// twoFer() => "One for you, one for me"
+// twoFer("Elton") => "One for Elton, one for me"
+function twoFer(personName) {
+    if (personName === void 0) { personName = "you"; }
+    return "one for ".concat(personName, ", one for me");
+}
+console.log(twoFer());
+console.log(twoFer("Elton"));
+// **********************************************
+// ******************* PART 2 *******************
+// **********************************************
+// Write a isLeapyear() function that accepts a year and returns true/false depending on if the year is a leap year
+// isLeapYear(2012) => true
+// isLeapYear(2013) => false
+// To determine whether a year is a leapyear, use this "formula":
+// A YEAR IS A LEAPYEAR IF
+// - year is a multiple of 4 AND not a multiple of 100
+// OR...
+// - year is a multiple of 400
+// hint - use modulo
+function isLeapYear(year) {
+    return (year % 4 === 0 && year % 100 !== 0) || year % 400 === 0;
+}
+console.log(isLeapYear(2012));
+console.log(isLeapYear(2013));
