7e0777274fe5f77f79d87826ab15a80a0a272cae
Simone Taeggi <startae14@gmail.com>
2022-08-17 10:28:58 +0200

    Create type Aliases

--- Objects/objects.ts
+++ Objects/objects.ts
@@ -5,15 +5,39 @@ function printName(person: { first: string; last: string }): void {
 printName({ first: "Thomas", last: "Jenkins" });
 
 // define an object variable
-let coordinate: { x: number; y: number } = { x: 34, y: 2 };
+// let coordinate: { x: number; y: number } = { x: 34, y: 2 };
 
 // define an object return
-function randomCoordinate(): { x: number; y: number } {
-    return { x: Math.random(), y: Math.random() };
-}
+// function randomCoordinate(): { x: number; y: number } {
+//     return { x: Math.random(), y: Math.random() };
+// }
 
 // Excess properties
 printName({ first: "Mick", last: "Jagger", age: 473 });
 
 const singer = { first: "Mick", last: "Jagger", age: 473, isAlive: true };
 printName(singer);
+
+// Type Alias
+type Point = {
+    x: number,
+    y: number
+};
+
+let coordinate: Point = { x: 34, y: 2 };
+
+// define an object return
+function randomCoordinate(): Point {
+    return { x: Math.random(), y: Math.random() };
+}
+
+function doublePoint(point: Point): Point {
+    return {
+        x: point.x * 2,
+        y: point.y * 2
+    };
+}
+
+// another Alias
+type MyNum = number;
+let age: MyNum = 24234;
