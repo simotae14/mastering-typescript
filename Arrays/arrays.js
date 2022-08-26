0f0dd4294b649ceec2b40dc2171865ac61571f97
Simone Taeggi <startae14@gmail.com>
2022-08-26 09:37:57 +0200

    Array of Complex Types

--- Arrays/arrays.js
+++ Arrays/arrays.js
@@ -5,3 +5,13 @@ var activeUsers = [];
 activeUsers.push('Tony');
 var ageList = [45, 56, 13];
 ageList[0] = 99;
+// array alternative syntax
+var bools = [];
+var cords = [];
+cords.push({
+    x: 23,
+    y: 8
+});
+// cords.push({
+//     y: 8
+// });
--- Arrays/arrays.ts
+++ Arrays/arrays.ts
@@ -7,3 +7,21 @@ activeUsers.push('Tony');
 
 const ageList: number[] = [45, 56, 13];
 ageList[0] = 99;
+
+// array alternative syntax
+const bools: Array<boolean> = [];
+
+// array of complex data
+type Point = {
+    x: number,
+    y: number
+};
+
+const cords: Point[] = [];
+cords.push({
+    x: 23,
+    y: 8
+});
+// cords.push({
+//     y: 8
+// });
