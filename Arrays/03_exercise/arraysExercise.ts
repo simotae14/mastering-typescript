e45f70a1e1c92d11b005dbe8c134e2013850e072
Simone Taeggi <startae14@gmail.com>
2022-08-26 09:56:46 +0200

    Exercise with Arrays

---
+++ Arrays/03_exercise/arraysExercise.ts
@@ -0,0 +1,36 @@
+// **********************************************
+// ******************* PART 1 *******************
+// **********************************************
+// Create an empty array of numbers called "ages":
+const ages: number[] = [];
+
+// **********************************************
+// ******************* PART 2 *******************
+// **********************************************
+// Create an array variable called gameBoard that starts as an empty array.
+// It should be typed to hold a 2 dimensional array of strings
+const gameBoard: string[][] = [];
+
+// **********************************************
+// ******************* PART 3 *******************
+// **********************************************
+// Create a Product type that contains a name and a price.
+// An example product could be:
+// {name: "coffee mug", price: 11.50}
+type Product = {
+    name: string,
+    price: number
+};
+// **********************************************
+// ******************* PART 4 *******************
+// **********************************************
+// Write a function called getTotal that accepts an array of Product types
+// It should return the sum of all the products' prices
+function getTotal(products: Product[]): number {
+    let total: number = 0;
+    products.forEach((product: Product): void => {
+        total = total + product.price;
+    });
+    return total;
+};
+
