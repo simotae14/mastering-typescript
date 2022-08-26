a1197176b91a41004c2420e902d9ee649b696fe0
Simone Taeggi <startae14@gmail.com>
2022-08-26 09:32:16 +0200

    Definition of simple Array Types

---
+++ Arrays/arrays.js
@@ -0,0 +1,7 @@
+// define an empty array
+// const activeUsers: [] = [];
+// define an array of strings
+var activeUsers = [];
+activeUsers.push('Tony');
+var ageList = [45, 56, 13];
+ageList[0] = 99;
---
+++ Arrays/arrays.ts
@@ -0,0 +1,9 @@
+// define an empty array
+// const activeUsers: [] = [];
+
+// define an array of strings
+const activeUsers: string[] = [];
+activeUsers.push('Tony');
+
+const ageList: number[] = [45, 56, 13];
+ageList[0] = 99;
