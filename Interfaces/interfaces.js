4bf8c4b3ec47ac03300e2324f858cd64fb883996
Simone Taeggi <startae14@gmail.com>
2022-09-15 14:50:16 +0200

    Readonly and Optional Interfaces properties

--- Interfaces/interfaces.js
+++ Interfaces/interfaces.js
@@ -1,1 +1,9 @@
 var pt = { x: 123, y: 1234 };
+var thomas = {
+    id: 21837,
+    first: "Thomas",
+    last: "Hardy",
+    nickname: 'Tom'
+};
+thomas.first = 'John';
+// thomas.id = 3456 give error
--- Interfaces/interfaces.ts
+++ Interfaces/interfaces.ts
@@ -4,3 +4,22 @@ interface Point {
 }
 
 const pt: Point = { x: 123, y: 1234 }
+
+interface Person {
+    // readonly pty
+    readonly id: number;
+    first: string;
+    last: string;
+    // optional
+    nickname?: string;
+}
+
+const thomas: Person = {
+    id: 21837,
+    first: "Thomas",
+    last: "Hardy",
+    nickname: 'Tom'
+}
+
+thomas.first = 'John';
+// thomas.id = 3456 give error
