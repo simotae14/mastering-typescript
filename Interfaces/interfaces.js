1475eb61e7adf02539a85efe6b880ed09e433821
Simone Taeggi <startae14@gmail.com>
2022-09-15 16:54:08 +0200

    Interfaces method parameters

--- Interfaces/interfaces.js
+++ Interfaces/interfaces.js
@@ -3,7 +3,19 @@ var thomas = {
     id: 21837,
     first: "Thomas",
     last: "Hardy",
-    nickname: 'Tom'
+    nickname: 'Tom',
+    sayHi: function () {
+        return "Hello!";
+    }
 };
 thomas.first = 'John';
-// thomas.id = 3456 give error
+var shoes = {
+    name: "Blue Suede Shoes",
+    price: 100,
+    applyDiscount: function (amount) {
+        var newPrice = this.price * (1 - amount);
+        this.price = newPrice;
+        return this.price;
+    }
+};
+console.log(shoes.applyDiscount(0.4));
--- Interfaces/interfaces.ts
+++ Interfaces/interfaces.ts
@@ -12,14 +12,36 @@ interface Person {
     last: string;
     // optional
     nickname?: string;
+    sayHi: () => string; // sayHi(): string;
 }
 
 const thomas: Person = {
     id: 21837,
     first: "Thomas",
     last: "Hardy",
-    nickname: 'Tom'
+    nickname: 'Tom',
+    sayHi: () => {
+        return "Hello!";
+    },
 }
 
 thomas.first = 'John';
 // thomas.id = 3456 give error
+
+interface Product {
+    name: string;
+    price: number;
+    applyDiscount(discount: number): number;
+}
+
+const shoes: Product = {
+    name: "Blue Suede Shoes",
+    price: 100,
+    applyDiscount(amount: number) {
+        const newPrice = this.price * (1 - amount);
+        this.price = newPrice;
+        return this.price;
+    }
+}
+
+console.log(shoes.applyDiscount(0.4));
