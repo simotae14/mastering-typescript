63eafdf668d7277de76e3c795d40e4afd83c6ffe
Simone Taeggi <startae14@gmail.com>
2022-09-20 17:48:09 +0200

    Tsconfig, target option

--- TypescriptCompiler/dist/farmstand.js
+++ TypescriptCompiler/dist/farmstand.js
@@ -1,4 +1,4 @@
 "use strict";
-const printProduct = (product) => {
-    console.log(`${product.name} - $${product.price}`);
+var printProduct = function (product) {
+    console.log("".concat(product.name, " - $").concat(product.price));
 };
--- TypescriptCompiler/dist/index.js
+++ TypescriptCompiler/dist/index.js
@@ -1,11 +1,11 @@
 "use strict";
-const norma = {
+var norma = {
     breed: 'Silkie',
     eggsPerWeek: 4,
     name: 'Norma',
     age: 2,
 };
-const juniper = {
+var juniper = {
     breed: 'Silkie',
     eggsPerWeek: 5,
     name: 'Juniper',
