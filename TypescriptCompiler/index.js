1270b21c65158e4dc18418fc97d8dee8425e4295
Simone Taeggi <startae14@gmail.com>
2022-09-20 12:35:38 +0200

    Basic TS config for a project

---
+++ TypescriptCompiler/index.js
@@ -0,0 +1,5 @@
+var norma = {
+    breed: 'Silkie',
+    eggsPerWeek: 4,
+    name: 'Norma '
+};
---
+++ TypescriptCompiler/index.ts
@@ -0,0 +1,11 @@
+interface Chicken {
+  breed: string;
+  eggsPerWeek: number;
+  name: string;
+}
+
+const norma: Chicken = {
+  breed: 'Silkie',
+  eggsPerWeek: 4,
+  name: 'Norma '
+}
