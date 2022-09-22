0ce77fafb2cf00efa81f24a1ce6d6598991ba94d
Simone Taeggi <startae14@gmail.com>
2022-09-22 17:54:50 +0200

    Build the Todo list

--- MiniProject/dist/index.js
+++ MiniProject/dist/index.js
@@ -2,8 +2,16 @@
 const btn = document.getElementById("btn");
 const input = document.getElementById("todoinput");
 const form = document.querySelector("form");
+const list = document.getElementById("todolist");
 function handleSubmit(e) {
     e.preventDefault();
-    console.log("SUBMITTED!");
+    const newTodoText = input.value;
+    const newLI = document.createElement("li");
+    const checkbox = document.createElement("input");
+    checkbox.type = "checkbox";
+    newLI.append(newTodoText);
+    newLI.append(checkbox);
+    list.append(newLI);
+    input.value = "";
 }
 form.addEventListener("submit", handleSubmit);
--- MiniProject/src/index.ts
+++ MiniProject/src/index.ts
@@ -1,10 +1,18 @@
 const btn = document.getElementById("btn")! as HTMLButtonElement;
 const input = document.getElementById("todoinput")! as HTMLInputElement;
 const form = document.querySelector("form")!;
+const list = document.getElementById("todolist")!;
 
 function handleSubmit(e: SubmitEvent) {
   e.preventDefault();
-  console.log("SUBMITTED!");
+  const newTodoText = input.value;
+  const newLI = document.createElement("li");
+  const checkbox = document.createElement("input");
+  checkbox.type = "checkbox";
+  newLI.append(newTodoText);
+  newLI.append(checkbox);
+  list.append(newLI);
+  input.value = "";
 }
 
 form.addEventListener("submit", handleSubmit);
