2cbcf9ae8dade94881b7376d52b10b24731217ca
Simone Taeggi <startae14@gmail.com>
2022-09-28 09:19:15 +0200

    Create an interface for the Todo

--- MiniProject/dist/index.js
+++ MiniProject/dist/index.js
@@ -3,15 +3,23 @@ const btn = document.getElementById("btn");
 const input = document.getElementById("todoinput");
 const form = document.querySelector("form");
 const list = document.getElementById("todolist");
+const todos = [];
 function handleSubmit(e) {
     e.preventDefault();
-    const newTodoText = input.value;
+    const newTodo = {
+        text: input.value,
+        completed: false
+    };
+    createTodo(newTodo);
+    todos.push(newTodo);
+    input.value = "";
+}
+function createTodo(todo) {
     const newLI = document.createElement("li");
     const checkbox = document.createElement("input");
     checkbox.type = "checkbox";
-    newLI.append(newTodoText);
+    newLI.append(todo.text);
     newLI.append(checkbox);
     list.append(newLI);
-    input.value = "";
 }
 form.addEventListener("submit", handleSubmit);
--- MiniProject/src/index.ts
+++ MiniProject/src/index.ts
@@ -1,18 +1,34 @@
+interface Todo {
+  text: string;
+  completed: boolean;
+}
+
 const btn = document.getElementById("btn")! as HTMLButtonElement;
 const input = document.getElementById("todoinput")! as HTMLInputElement;
 const form = document.querySelector("form")!;
 const list = document.getElementById("todolist")!;
 
+const todos: Todo[] = [];
+
 function handleSubmit(e: SubmitEvent) {
   e.preventDefault();
-  const newTodoText = input.value;
+  const newTodo: Todo = {
+    text: input.value,
+    completed: false
+  };
+  createTodo(newTodo);
+  todos.push(newTodo);
+  input.value = ""; 
+}
+
+function createTodo(todo: Todo) {
   const newLI = document.createElement("li");
   const checkbox = document.createElement("input");
   checkbox.type = "checkbox";
-  newLI.append(newTodoText);
+  newLI.append(todo.text);
   newLI.append(checkbox);
   list.append(newLI);
-  input.value = "";
+  
 }
 
 form.addEventListener("submit", handleSubmit);
