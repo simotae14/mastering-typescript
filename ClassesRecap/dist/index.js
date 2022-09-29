4b15a1fec6ef06e7e7f04a50aa4a100cf0193f5c
Simone Taeggi <startae14@gmail.com>
2022-09-29 15:13:50 +0200

    Constructor

--- ClassesRecap/dist/index.js
+++ ClassesRecap/dist/index.js
@@ -1,12 +1,17 @@
 class Player {
+  constructor(first, last) {
+    this.first = first;
+    this.last = last;
+  }
   taunt() {
     console.log("BOOYA!");
   }
 }
 
-const player1 = new Player();
+const player1 = new Player("blue", "steele ");
 player1.taunt();
-console.log(player1);
+console.log(player1.first);
+console.log(player1.last );
 
-const player2 = new Player();
+const player2 = new Player("charlie", "brown");
 player2.taunt();
