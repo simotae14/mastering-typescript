ac138314f7a430d7c7c7cb4388b78b0cdb88cc91
Simone Taeggi <startae14@gmail.com>
2022-09-29 14:18:45 +0200

    Defining a class

--- ClassesRecap/dist/index.js
+++ ClassesRecap/dist/index.js
@@ -1,3 +1,12 @@
-"use strict";
-console.log("WOOOO!!!");
-console.log("YAYAYAYA!!!");
+class Player {
+  taunt() {
+    console.log("BOOYA!");
+  }
+}
+
+const player1 = new Player();
+player1.taunt();
+console.log(player1);
+
+const player2 = new Player();
+player2.taunt();
