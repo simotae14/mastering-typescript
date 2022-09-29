643e052320a3b3d28509436b5276b8e2eebcfeb1
Simone Taeggi <startae14@gmail.com>
2022-09-29 16:32:48 +0200

    Setters

--- ClassesRecap/dist/index.js
+++ ClassesRecap/dist/index.js
@@ -9,8 +9,19 @@ class Player {
   get fullName() {
     return `${this.first} ${this.last}`;
   }
+  set fullName(newName) {
+    const [first, last] = newName.split(' ');
+    this.first = first;
+    this.last = last;
+  }
   get score() {
     return this.#score;
+  }
+  set score(newScore) {
+    if (newScore < 0) {
+      throw new Error("Score must be positive!"); 
+    }
+    this.#score = newScore;
   }
   getScore() {
     return this.#score;
@@ -41,8 +52,14 @@ const player1 = new Player("blue", "steele ");
 // console.log(player1.getScore());
 // console.log(player1.setScore(28));
 // console.log(player1.getScore());
-console.log(player1.score);
+// console.log(player1.score);
+// player1.score = 335;
+// console.log(player1.score);
 
 // const player2 = new Player("charlie", "brown");
 // player2.taunt();
 // player1.#secret();
+
+console.log(player1.fullName);
+player1.fullName = "Amy Adams";
+console.log(player1.fullName);
