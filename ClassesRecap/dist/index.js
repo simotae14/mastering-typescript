f759f7f321c196e701b72fdec9c7911e81045ae3
Simone Taeggi <startae14@gmail.com>
2022-09-29 15:38:05 +0200

    Private Fields and Methods

--- ClassesRecap/dist/index.js
+++ ClassesRecap/dist/index.js
@@ -1,15 +1,25 @@
 class Player {
-  score = 0;
-  numLives = 10;
+  #score = 0;
+  #numLives = 10;
   constructor(first, last) {
     this.first = first;
     this.last = last;
+    this.#secret();
+  }
+  getScore() {
+    return this.#score;
+  }
+  setScore(newScore) {
+    this.#score = newScore;
   }
   taunt() {
     console.log("BOOYA!");
   }
   loseLife() {
-    this.numLives -= 1;
+    this.#numLives -= 1;
+  }
+  #secret() {
+    console.log("SECRET!!!");
   }
 }
 
@@ -18,11 +28,15 @@ const player1 = new Player("blue", "steele ");
 // console.log(player1.first);
 // console.log(player1.last);
 // console.log(player1);
-console.log(player1.numLives);
+// console.log(player1.#numLives);
 player1.loseLife();
-console.log(player1.numLives);
-player1.score = -124293;
+// console.log(player1.numLives);
+// player1.#score = -124293;
+console.log(player1.getScore());
+console.log(player1.setScore(28));
+console.log(player1.getScore());
 console.log(player1);
 
 // const player2 = new Player("charlie", "brown");
 // player2.taunt();
+// player1.#secret();
