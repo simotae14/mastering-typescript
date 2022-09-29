a7adafe2d941ec7d1d49f80415b16340ed874129
Simone Taeggi <startae14@gmail.com>
2022-09-29 15:28:03 +0200

    Class Fields

--- ClassesRecap/dist/index.js
+++ ClassesRecap/dist/index.js
@@ -1,4 +1,6 @@
 class Player {
+  score = 0;
+  numLives = 10;
   constructor(first, last) {
     this.first = first;
     this.last = last;
@@ -6,12 +8,21 @@ class Player {
   taunt() {
     console.log("BOOYA!");
   }
+  loseLife() {
+    this.numLives -= 1;
+  }
 }
 
 const player1 = new Player("blue", "steele ");
-player1.taunt();
-console.log(player1.first);
-console.log(player1.last );
+// player1.taunt();
+// console.log(player1.first);
+// console.log(player1.last);
+// console.log(player1);
+console.log(player1.numLives);
+player1.loseLife();
+console.log(player1.numLives);
+player1.score = -124293;
+console.log(player1);
 
-const player2 = new Player("charlie", "brown");
-player2.taunt();
+// const player2 = new Player("charlie", "brown");
+// player2.taunt();
